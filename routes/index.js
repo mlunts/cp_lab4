32abfaff08b572f6e152740d4b074d4a8ba78031
earine <marinalunts@gmail.com>
2020-04-10 23:00:25 +0300

    Updated json file; added routes for adding books and filter

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,63 @@ const fs = require('fs');
 const uuidv4 = require('uuid/v4');
 
 const json_books = fs.readFileSync('./books.json', 'utf-8');
+
+var today = new Date();
 let books = JSON.parse(json_books);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express', books: books});
+});
+
+router.post('/filter', function(req, res) {
+  
+  books = JSON.parse(json_books);
+
+  let filteredBooks = []
+  let dueDate = req.body.dueDate
+
+    for (let index = 0; index < Object.keys(books).length; index++) {
+        let book = books[index]
+        let bookDueDate = new Date(book['inLibrary']['due']);
+  
+        if (bookDueDate < new Date(dueDate)) {
+          filteredBooks.push(books[index])
+        }  
+      }
+  
+      books = filteredBooks
+  
+  
+  res.redirect('/')
+});
+
+router.post('/showAll', function(req, res) {
+  books = JSON.parse(json_books);
+  res.redirect('/')
+});
+
+router.post('/inStock', function(req, res) {
+  
+  books = JSON.parse(json_books);
+
+  let filteredBooks = []
+  let inStock = req.body.inLibrary == 'on'
+
+  if (inStock) {
+    
+    for (let index = 0; index < Object.keys(books).length; index++) {
+        let book = books[index]
+
+        if (book['inLibrary']['status'] == true) {
+          filteredBooks.push(books[index])
+        }
+    }
+  
+      books = filteredBooks
+  }
+  
+  res.redirect('/')
 });
 
 router.post('/add-book', (req, res) => {
